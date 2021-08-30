3bc91b836ae196d139c028d459f152971a6656f7
Anurag Singh <63997049+anuragc10@users.noreply.github.com>
2021-08-31 00:07:45 +0530

    Create kth smallest element.py

---
+++ gfg/kth smallest element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def kthSmallest(self,arr, l, r, k):
+        '''
+        arr : given array
+        l : starting index of the array i.e 0
+        r : ending index of the array i.e size-1
+        k : find kth smallest element and return using this function
+        '''
+        
+        s=list(set(arr))
+        s.sort()
+        return s[k-1]
+        

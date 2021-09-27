7868488ca87ce51d1cb1fb1ca3366b36a6c262c1
Anurag Singh <63997049+anuragc10@users.noreply.github.com>
2021-09-27 20:57:45 +0530

    Create Remove duplicate element from sorted Linked List .cpp

---
+++ gfg/Remove duplicate element from sorted Linked List .cpp
@@ -0,0 +1,16 @@
+void removeDuplicates(struct Node* head)
+{
+    
+    while(head->next!=NULL)
+    {
+        if(head->data==head->next->data)
+        {
+            head->next=head->next->next;
+        }
+        else
+        {
+            head=head->next;
+        }
+    }
+    
+}

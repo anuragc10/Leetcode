10d6be5800470d7d5f42d0f14031045f2800dbd9
Anurag Singh <63997049+anuragc10@users.noreply.github.com>
2022-02-22 23:24:32 +0530

    Create count of subset sum with given sum.cpp

---
+++ DP/count of subset sum with given sum.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int CountSubset(int arr[],int W,int n)
+{
+    int t[n+1][W+1];
+    for(int i=0;i<n+1;i++)
+    {
+        for(int j=0;j<W+1;j++)
+        {
+            if(i==0)
+            {
+                t[i][j]=0;
+            }
+            if(j==0)
+            {
+                t[i][j]=1;
+            }
+        }
+    }
+    for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<W+1;j++)
+            {
+                if(arr[i-1]<=j)
+                {
+                    t[i][j]=t[i-1][j-arr[i-1]] + t[i-1][j];
+                }
+                else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        return t[n][W];
+}
+
+int main()
+{
+    int arr[]={2,3,5,6,8,10};
+    int s=10;
+    int n=6;
+    cout<<CountSubset(arr,s,n);
+}
